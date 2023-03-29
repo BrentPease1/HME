c4f7a847b18eb6dffbd0c64fda072a643e7830f5
Brent <bspease@ncsu.edu>
2023-03-29 12:03:09 -0500

    n-mix

--- Modules/04_n_mixture_models/02_GoF.R
+++ Modules/04_n_mixture_models/02_GoF.R
@@ -30,89 +30,56 @@ for(j in 1:J){
   C[,j] <- rbinom(n = M, size = N, prob = det.prob)
 }
 
-# Take a look at truth
-table(N)
-
-# total number of individuals in population
-sum(N) #why this number?
-
-# Number of occupied sites
-sum(N > 0)
-
-# mean abundance
-mean(N)
-
-## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-# Here is our observation of truth
-table(apply(C,1,max))
-
-# observed population size
-sum(apply(C,1,max))
-
-# Observed number of occupied sites
-sum(apply(C,1,max) >0)
-
-# observed mean abundance
-mean(apply(C,1,max))
-
-# comparison of truth vs observations
-head(cbind(N=N, count1=C[,1], count2=C[,2]))
-
-## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-# FIT MODELS
-
-# unmarked is workhorse for a range of models
-# https://cran.r-project.org/web/packages/unmarked/vignettes/unmarked.html
-
-# fit n-mixture models using pcount function
-
-# create data frame that library can ingest
-umf <- unmarkedFramePCount(y = C)
-summary(umf)
-
-# fit model, get estimates on link scale
-# specify linear model of each submodel
-# observation is first, ecological is second
-m1 <- pcount(~1 ~1, data = umf)
-
-# get estimates on natural scale
-backTransform(m1, "state")
-backTransform(m1, "det")
-
-# Do estimates reflect truth?
-
 ## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 ## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 
-# N-mix in Nimble
 
 nmix1 <- nimbleCode({
-  
+
   # Priors
   lambda ~ dgamma(0.01,0.01)
   p ~ dbeta(1,1)
   
-  # likelihood
-  for(i in 1:M){
-    N[i] ~ dpois(lambda)           # state model
-    for(j in 1:J){
-      C[i,j] ~ dbin(p, N[i])       # observation model
+  # Likelihood
+  for (i in 1:M) {
+    N[i] ~ dpois(lambda)      # State model
+    for (j in 1:J) {
+      C[i,j] ~ dbin(p, N[i]) # Observation model
     }
   }
   
   # derived values of interest
   Ntotal <- sum(N[1:M])
+  
+  # Posterior predictive distributions of chi2 discrepancy
+  for (i in 1:M) {
+    for (j in 1:J) {
+      C.sim[i,j] ~ dbin(p, N[i]) # Create new data set under model
+      e.count[i,j] <- N[i] * p   # Expected Count
+      
+      # Add small value e to denominator to avoid division by zero (e = 0.001)
+      # Chi-square discrepancy for the actual data
+      chi2.actual[i,j] <- pow((C[i,j]-e.count[i,j]),2) / (e.count[i,j]+e)
+      # Chi-square discrepancy for the simulated ('perfect') data
+      chi2.sim[i,j] <- pow((C.sim[i,j]-e.count[i,j]),2) / (e.count[i,j]+e)
+      
+    }
+  }
+  # Add up individual chi2 values for overall fit statistic
+  fit.actual <- sum(chi2.actual[1:M,1:J])  # Fit statistic for actual data set
+  fit.sim <- sum(chi2.sim[1:M,1:J])        # Fit statistic for a fitting model
+  c.hat <- fit.actual / fit.sim      # Over-dispersion measure (c-hat) - happens when variance is >> than mean 
+  bpv <- step(fit.sim-fit.actual)    # Bayesian p-value
+
+  
 })
 
 
 nimData <- list(C = C)
 nimConsts <- list(M = M,
-                  J = J) 
+                  J = J,
+                  e = 0.001) 
 
 # THIS IS NEW
 # N is a latent state variable - there is some true but unknown N at each site
@@ -125,7 +92,7 @@ nimInits <- list(N = Nst,
                  lambda = rgamma(1,0.1, 0.1),
                  p = rbeta(1,1,1))
 
-keepers <- c('lambda', 'p', 'Ntotal')
+keepers <- c('lambda', 'p', 'Ntotal', 'c.hat', 'bpv')
 
 m2 <- nimbleMCMC(code = nmix1,
                  data = nimData,
@@ -145,8 +112,4 @@ params_of_interest <- c("lambda", "p")
 mcmcplot(m2$samples[,params_of_interest])
 
 # inspect output
-m2$summary
-
-
-# GOODNESS OF FIT FOR 
-
+m2$summary$all.chains
